4faaeb50e30a46cfe81d400e000dc1dba10367bd
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-02-03 09:57:03 +0700

    tanggal

---
+++ assets/js/count-timer.js
@@ -0,0 +1,27 @@
+// Tetapkan tanggal kita menghitung mundur
+var countDownDate = new Date("july 27, 2023 10:00:20").getTime();
+
+// Perbarui hitungan mundur setiap 1 detik
+var x = setInterval(function () {
+  // Dapatkan tanggal dan waktu hari ini
+  var now = new Date().getTime();
+
+  // Temukan jarak antara sekarang dan tanggal hitung mundur
+  var distance = countDownDate - now;
+
+  // Perhitungan waktu untuk hari, jam, menit dan detik
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Keluarkan hasil dalam elemen dengan id = "demo"
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  //Jika hitungan mundur selesai, tulis beberapa teks
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);

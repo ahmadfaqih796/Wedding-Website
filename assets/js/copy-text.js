847105ea8a68fc853db6094abdb6c6dd1e97911c
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-02-07 21:51:54 +0700

    fix copy

--- assets/js/copy-text.js
+++ assets/js/copy-text.js
@@ -1,10 +1,22 @@
-let text = document.getElementById("copy").innerHTML;
-const copyRekening = async () => {
+let textBCA = document.getElementById("copy-bca").innerHTML;
+const copyRekeningBCA = async () => {
   try {
-    await navigator.clipboard.writeText(text);
-    console.log("Berhasil mengcopy " + text);
-    alert("Berhasil mengcopy " + text)
+    await navigator.clipboard.writeText(textBCA);
+    console.log("Berhasil mengcopy " + textBCA);
+    alert("Berhasil mengcopy " + textBCA)
   } catch (err) {
     console.error("Failed to copy: ", err);
   }
 };
+
+let textBRI = document.getElementById("copy-bri").innerHTML;
+const copyRekeningBRI = async () => {
+  try {
+    await navigator.clipboard.writeText(textBRI);
+    console.log("Berhasil mengcopy " + textBRI);
+    alert("Berhasil mengcopy " + textBRI)
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+

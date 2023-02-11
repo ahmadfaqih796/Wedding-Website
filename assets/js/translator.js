0f8c80eca4409999ce901accb75bbe7d289d1c2c
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-02-11 23:33:42 +0700

    style translate

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,6 +1,11 @@
 function googleTranslateElementInit() {
-   new google.translate.TranslateElement(
-     { pageLanguage: "id" },
-     "google_translate_element"
-   );
- }
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "id",
+      includedLanguages: "id,ar,en,es,jv,ko,pa,pt,ru,zh-CN",
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+      autoDisplay: false,
+    },
+    "google_translate_element"
+  );
+}

41af6ec68585a2e40697fe49485d33491431c442
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-02-12 22:34:25 +0700

    fix musik js

--- assets/js/musik.js
+++ assets/js/musik.js
@@ -1,6 +1,6 @@
 var musik = document.getElementById("musik");
 var icon = document.getElementById("icon");
-musik.loop = true
+musik.loop = true;
 // Creating a function that will change
 // pause to play and vice-versa
 icon.onclick = function () {
@@ -8,12 +8,18 @@ icon.onclick = function () {
     // If paused then play the
     // music and change the image
     musik.play();
-    icon.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    icon.innerHTML = `<i class="fa-solid fa-pause"> </i><marquee>Tulus - Teman Hidup</marquee>`;
+    icon.style.width = "100px";
+    icon.style.justifyContent = "center"
+    icon.style.transition= "0.5s all"
     //  icon.src = "nama file gambar";
   } else {
     // If playing then pause the
     // music and change the image
     musik.pause();
     icon.innerHTML = `<i class="fa-solid fa-play"></i>`;
+    icon.style.width = "40px";
+    icon.style.justifyContent = "left"
+    icon.style.paddingLeft="4px"
   }
 };
